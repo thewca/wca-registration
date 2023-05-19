b33be1b79b09bfdbbdf3841b6f5a128c66a8bdb7
Duncan <duncanonthejob@gmail.com>
2023-05-19 13:48:28 +0200

    added support for shaped load tests

--- load_testing/poc_locustfile.py
+++ load_testing/poc_locustfile.py
@@ -1,4 +1,5 @@
 from locust import HttpUser, task, between
+from locust import LoadTestShape
 import random
 import csv
 import time
@@ -17,13 +18,13 @@ class TestUser(HttpUser):
     def on_start(self):
         self.endpoint = "//register"
         self.registered = False
-        self.wca_id = wca_ids.pop(random.randint(0, len(wca_ids)))
         
 
     @task
     def submit_registration(self):
-        while self.registered:
-            time.sleep(1)
+        self.wca_id = wca_ids.pop(random.randint(0, len(wca_ids)))
+        # while self.registered:
+        #     time.sleep(1)
 
         # registration_data = {
         #         "competitor_id":self.wca_id,
@@ -60,3 +61,51 @@ class TestUser(HttpUser):
 
         # self.client.post(f"{self.endpoint}", headers=headers, data=registration_data)
         self.registered = True
+
+
+
+
+
+class StagesShape(LoadTestShape):
+    """
+    A simply load test shape class that has different user and spawn_rate at
+    different stages.
+
+    Keyword arguments:
+
+        stages -- A list of dicts, each representing a stage with the following keys:
+            duration -- When this many seconds pass the test is advanced to the next stage
+            users -- Total user count
+            spawn_rate -- Number of users to start/stop per second
+            stop -- A boolean that can stop that test at a specific stage
+
+        stop_at_end -- Can be set to stop once all stages have run.
+    """
+
+    stages = [
+        {"duration": 60, "users": 6, "spawn_rate": 1},
+        {"duration": 120, "users": 13, "spawn_rate": 1},
+        {"duration": 180, "users": 20, "spawn_rate": 1},
+        {"duration": 240, "users": 26, "spawn_rate": 1},
+        {"duration": 300, "users": 33, "spawn_rate": 1},
+        {"duration": 360, "users": 40, "spawn_rate": 1},
+        {"duration": 420, "users": 46, "spawn_rate": 1},
+        {"duration": 480, "users": 53, "spawn_rate": 1},
+        {"duration": 540, "users": 60, "spawn_rate": 1},
+        {"duration": 600, "users": 66, "spawn_rate": 1},
+        {"duration": 660, "users": 73, "spawn_rate": 1},
+        {"duration": 720, "users": 80, "spawn_rate": 1},
+        {"duration": 780, "users": 86, "spawn_rate": 1},
+        {"duration": 840, "users": 93, "spawn_rate": 1},
+        {"duration": 900, "users": 100, "spawn_rate": 1},
+    ]
+
+    def tick(self):
+        run_time = self.get_run_time()
+
+        for stage in self.stages:
+            if run_time < stage["duration"]:
+                tick_data = (stage["users"], stage["spawn_rate"])
+                return tick_data
+
+        return None
