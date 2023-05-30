701512e2fc18ff5d19cb321858f6fe0709af5696
Finn Ickler <finnickler@gmail.com>
2023-05-30 19:12:55 +0200

    upgraded WCA Component library to v.0.1.1

--- Frontend/src/index.jsx
+++ Frontend/src/index.jsx
@@ -1,6 +1,9 @@
 import { createRoot } from 'react-dom/client'
 import App from './register/pages'
 
+// External Styles (this is probably not the best way to load this?)
+import '@thewca/wca-components/dist/index.esm.css'
+
 // Clear the existing HTML content
 document.body.innerHTML = '<div id="app"></div>'
 
