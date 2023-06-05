b77488ce5a2f910aba090603ac4d18845a2544ce
FinnIckler <finnickler@gmail.com>
2023-06-05 16:22:04 +0200

    Switch Frontend -> Backend Communication to JSON (#55)
    Changed over communication between the frontend and the backend to json and switched the API functions to typescript so the contracts can be generated from swagger

--- Frontend/.eslintrc.js
+++ Frontend/.eslintrc.js
@@ -1,18 +1,19 @@
 const { configure, presets } = require('eslint-kit')
 
 module.exports = configure({
-    mode: 'only-errors',
-    presets: [
-        presets.imports(),
-        presets.prettier(),
-        presets.node(),
-        presets.react(),
-    ],
-    extend: {
-        rules: {
-            'import/no-default-export': 'off',
-            "react/jsx-uses-vars": "error",
-            "react/jsx-uses-react": "error"
-        },
-    }
+  mode: 'only-errors',
+  presets: [
+    presets.imports(),
+    presets.prettier(),
+    presets.node(),
+    presets.react(),
+    presets.typescript(),
+  ],
+  extend: {
+    rules: {
+      'import/no-default-export': 'off',
+      'react/jsx-uses-vars': 'error',
+      'react/jsx-uses-react': 'error',
+    },
+  },
 })
---
+++ Frontend/src/api/helper/backend_fetch.ts
@@ -0,0 +1,41 @@
+import {
+  DeleteRegistrationBody,
+  GetRegistrationBody,
+  SubmitRegistrationBody,
+  UpdateRegistrationBody,
+} from '../types'
+
+type Method = 'POST' | 'GET' | 'PATCH' | 'DELETE'
+
+type Body =
+  | SubmitRegistrationBody
+  | UpdateRegistrationBody
+  | GetRegistrationBody
+  | DeleteRegistrationBody
+
+export default async function backendFetch(
+  route: string,
+  method: Method,
+  body: Body = {}
+) {
+  try {
+    let init = {}
+    if (method !== 'GET') {
+      init = {
+        method,
+        body: JSON.stringify(body),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    }
+    const response = await fetch(`${process.env.API_URL}/${route}`, init)
+
+    if (response.ok) {
+      return await response.json()
+    }
+    return { error: response.statusText, statusCode: response.status }
+  } catch (error) {
+    return { error, statusCode: 500 }
+  }
+}
