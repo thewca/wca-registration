694ba71f6a9449d2a68439b70fea0080fd3e0c5b
Duncan <duncanonthejob@gmail.com>
2023-06-06 14:02:07 +0200

    added back tests

--- config/initializers/webmock.rb
+++ config/initializers/webmock.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
 # if Rails.env.test? || Rails.env.development?
 #   require 'webmock'
 #   WebMock.disable_net_connect!(allow_localhost: true)
--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -19,7 +19,7 @@ RSpec.configure do |config|
       openapi: '3.0.1',
       info: {
         title: 'API V1',
-        version: 'v1',
+        version: 'v1'
       },
       components: {
         schemas: {
@@ -32,7 +32,7 @@ RSpec.configure do |config|
               user_id: { type: :string },
               is_attending: { type: :boolean },
               lane_states: {
-                type: :object,
+                type: :object
               },
               lanes: {
                 type: :array,
@@ -42,10 +42,10 @@ RSpec.configure do |config|
                     lane_name: { type: :string },
                     lane_state: { type: :string },
                     completed_steps: {
-                      type: :array,
+                      type: :array
                     },
                     lane_details: {
-                      type: :object,
+                      type: :object
                     },
                     payment_reference: { type: :string },
                     payment_amount: { type: :string },
@@ -54,19 +54,19 @@ RSpec.configure do |config|
                     discount_amount: { type: :string },
                     last_action: { type: :string },
                     last_action_datetime: { type: :string, format: :date_time },
-                    last_action_user: { type: :string },
+                    last_action_user: { type: :string }
                   },
                   required: [:lane_name, :lane_state, :completed_steps, :lane_details,
                              :payment_reference, :payment_amount, :transaction_currency,
-                             :last_action, :last_action_datetime, :last_action_user],
-                },
+                             :last_action, :last_action_datetime, :last_action_user]
+                }
               },
-              hide_name_publicly: { type: :boolean },
+              hide_name_publicly: { type: :boolean }
             },
             required: [:attendee_id, :competition_id, :user_id, :is_attending, :lane_states,
-                       :lanes, :hide_name_publicly],
-          },
-        },
+                       :lanes, :hide_name_publicly]
+          }
+        }
 
       },
       paths: {},
@@ -75,12 +75,12 @@ RSpec.configure do |config|
           url: 'https://{defaultHost}',
           variables: {
             defaultHost: {
-              default: 'www.example.com',
-            },
-          },
-        },
-      ],
-    },
+              default: 'www.example.com'
+            }
+          }
+        }
+      ]
+    }
   }
 
   # Specify the format of the output Swagger file when running 'rswag:specs:swaggerize'.
