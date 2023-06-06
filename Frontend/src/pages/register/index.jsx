be7757c0cf41d4bc31e8306a158510cc4b566cb1
FinnIckler <finnickler@gmail.com>
2023-06-06 14:35:22 +0200

    Add Fomantic UI (#64)
    Add Fomantic UI to the frontend

--- Frontend/src/pages/register/index.jsx
+++ Frontend/src/pages/register/index.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import RegistrationPanel from './components/RegistrationPanel'
 import styles from './index.module.scss'
 
@@ -7,9 +6,6 @@ export default function Register() {
   return (
     <div className={styles.container}>
       <RegistrationPanel />
-      <div className={styles.link}>
-        <Link to="/registrations"> Registration List </Link>
-      </div>
     </div>
   )
 }
