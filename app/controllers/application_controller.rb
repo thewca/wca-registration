4b58e1a60892a583208142a97a7ee52ef94ac3a0
Finn Ickler <finnickler@gmail.com>
2023-06-19 10:01:47 +0200

    Setting the header to html for the profile report

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,12 @@ class ApplicationController < ActionController::API
   around_action :performance_profile if Rails.env == 'development'
 
   def performance_profile
-    if params[:profile] && result = RubyProf.profile { yield }
+    if params[:profile] && (result = RubyProf.profile { yield })
 
       out = StringIO.new
       RubyProf::GraphHtmlPrinter.new(result).print out, :min_percent => 0
-      self.response_body = out.string
-
+      response.set_header("Content-Type", "text/html")
+      response.body = out.string
     else
       yield
     end
