f1d5c4a7edcdc9ed1dc470dde31b49caf93b9fc1
Finn Ickler <finnickler@gmail.com>
2023-06-19 13:12:28 +0200

    Remove unrelated changes

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -13,11 +13,11 @@ class RegistrationController < ApplicationController
     @queue_url = ENV["QUEUE_URL"] || $sqs.get_queue_url(queue_name: 'registrations.fifo').queue_url
     # TODO: Cache this call?
     lane_created = begin
-      Registrations.find("#{competition_id}-#{user_id}")
-      true
-    rescue Dynamoid::Errors::RecordNotFound
-      false
-    end
+                     Registrations.find("#{competition_id}-#{user_id}")
+                     true
+                   rescue Dynamoid::Errors::RecordNotFound
+                     false
+                   end
     unless lane_created
       step_data = {
         user_id: user_id,
@@ -38,7 +38,6 @@ class RegistrationController < ApplicationController
     competitor_id = params[:competitor_id]
     competition_id = params[:competition_id]
     event_ids = params[:event_ids]
-    comment = params[:comment] || ""
 
     unless validate_request(competitor_id, competition_id) && !event_ids.empty?
       Metrics.registration_validation_errors_counter.increment
@@ -55,7 +54,6 @@ class RegistrationController < ApplicationController
       step_details: {
         registration_status: 'waiting',
         event_ids: event_ids,
-        comment: comment,
       },
     }
 
@@ -73,10 +71,8 @@ class RegistrationController < ApplicationController
     user_id = params[:competitor_id]
     competition_id = params[:competition_id]
     status = params[:status]
-    comment = params[:comment]
-    event_ids = params[:event_ids]
 
-    unless validate_request(user_id, competition_id)
+    unless validate_request(user_id, competition_id, status)
       Metrics.registration_validation_errors_counter.increment
       return render json: { status: 'User cannot register, wrong format' }, status: :forbidden
     end
@@ -84,24 +80,12 @@ class RegistrationController < ApplicationController
       registration = Registrations.find("#{competition_id}-#{user_id}")
       updated_lanes = registration.lanes.map { |lane|
         if lane.name == "Competing"
-          if status.present?
-            lane.lane_state = status
-          end
-          if comment.present?
-            lane.step_details["comment"] = comment
-          end
-          if event_ids.present?
-            lane.step_details["event_ids"] = event_ids
-          end
+          lane.lane_state = status
         end
         lane
       }
-      if status == "accepted"
-        registration.update_attributes(lanes: updated_lanes, is_attending: true)
-      else
-        registration.update_attributes(lanes: updated_lanes)
-      end
-
+      puts updated_lanes.to_json
+      registration.update_attributes(lanes: updated_lanes)
       # Render a success response
       render json: { status: 'ok' }
     rescue StandardError => e
@@ -110,27 +94,6 @@ class RegistrationController < ApplicationController
       Metrics.registration_dynamodb_errors_counter.increment
       render json: { status: "Error Updating Registration: #{e.message}" },
              status: :internal_server_error
-    end
-  end
-
-  def entry
-    user_id = params[:user_id]
-    competition_id = params[:competition_id]
-    unless validate_request(user_id, competition_id)
-      Metrics.registration_validation_errors_counter.increment
-      return render json: { status: "Can't get registration, wrong format" }, status: :forbidden
-    end
-    begin
-      registration = Registrations.find("#{competition_id}-#{user_id}")
-      render json: { registration: {
-        user_id: registration["user_id"],
-        event_ids: registration["lanes"][0].step_details["event_ids"],
-        registration_status: registration["lanes"][0].lane_state,
-        registered_on: registration["created_at"],
-        comment: registration["lanes"][0].step_details["comment"],
-      }, status: 'ok' }
-    rescue Dynamoid::Errors::RecordNotFound
-      render json: { registration: {}, status: 'ok' }
     end
   end
 
@@ -165,20 +128,6 @@ class RegistrationController < ApplicationController
 
   def list
     competition_id = params[:competition_id]
-    registrations = get_registrations(competition_id, only_attending: true)
-
-    # Render a success response
-    render json: registrations
-  rescue StandardError => e
-    # Render an error response
-    puts e
-    Metrics.registration_dynamodb_errors_counter.increment
-    render json: { status: "Error getting registrations" },
-           status: :internal_server_error
-  end
-
-  def list_admin
-    competition_id = params[:competition_id]
     registrations = get_registrations(competition_id)
 
     # Render a success response
@@ -193,52 +142,38 @@ class RegistrationController < ApplicationController
 
   private
 
-    REGISTRATION_STATUS = %w[waiting accepted deleted].freeze
+  REGISTRATION_STATUS = %w[waiting accepted].freeze
 
-    def user_exists(competitor_id)
-      Rails.cache.fetch(competitor_id, expires_in: 12.hours) do
-        CompetitorApi.check_competitor(competitor_id)
+  def user_exists(competitor_id)
+    Rails.cache.fetch(competitor_id, expires_in: 12.hours) do
+      CompetitorApi.check_competitor(competitor_id)
+    end
+  end
+
+  def competition_open(competition_id)
+    Rails.cache.fetch(competition_id, expires_in: 5.minutes) do
+      CompetitionApi.check_competition(competition_id)
+    end
+  end
+
+  def can_user_register?(competitor_id, competition_id)
+    # Check if user exists
+    user_exists(competitor_id) and competition_open(competition_id)
+  end
+
+  def validate_request(competitor_id, competition_id, status = 'waiting')
+    if competitor_id.present? && competitor_id =~ (/^\d{4}[a-zA-Z]{4}\d{2}$/)
+      if competition_id =~ (/^[a-zA-Z]+\d{4}$/) && REGISTRATION_STATUS.include?(status)
+        can_user_register?(competitor_id, competition_id)
       end
+    else
+      false
     end
+  end
 
-    def competition_open(competition_id)
-      Rails.cache.fetch(competition_id, expires_in: 5.minutes) do
-        CompetitionApi.check_competition(competition_id)
-      end
-    end
-
-    def can_user_register?(competitor_id, competition_id)
-      # Check if user exists
-      user_exists(competitor_id) and competition_open(competition_id)
-    end
-
-    def validate_request(competitor_id, competition_id, status = 'waiting')
-      if competitor_id.present? && competitor_id.to_s =~ (/\A\d+\z/)
-        if competition_id =~ (/^[a-zA-Z]+\d{4}$/) && REGISTRATION_STATUS.include?(status)
-          can_user_register?(competitor_id, competition_id)
-        end
-      else
-        false
-      end
-    end
-
-    def get_registrations(competition_id, only_attending: false)
-      # Query DynamoDB for registrations with the given competition_id using the Global Secondary Index
-      # TODO make this more beautiful and not break if there are more then one lane
-      # This also currently breaks if a registration is started but never completed
-      if only_attending
-        Registrations.where(competition_id: competition_id, is_attending: true).all.map do |x|
-          { competitor_id: x["user_id"],
-            event_ids: x["lanes"][0].step_details["event_ids"] }
-        end
-      else
-        Registrations.where(competition_id: competition_id).all.map do |x|
-          { competitor_id: x["user_id"],
-            event_ids: x["lanes"][0].step_details["event_ids"],
-            registration_status: x["lanes"][0].lane_state,
-            registered_on: x["created_at"],
-            comment: x["lanes"][0].step_details["comment"] }
-        end
-      end
-    end
+  def get_registrations(competition_id)
+    # Query DynamoDB for registrations with the given competition_id using the Global Secondary Index
+    # TODO make this more beautiful and not break if there are more then one lane
+    Registrations.where(competition_id: competition_id).all.map { |x| { competitor_id: x["user_id"], event_ids: x["lanes"][0].step_details["event_ids"], registration_status: x["lanes"][0].lane_state } }
+  end
 end
