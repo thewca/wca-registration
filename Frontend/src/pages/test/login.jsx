0281938e8f68c7006d0b2c32b0036c33ad490a16
Finn Ickler <finnickler@gmail.com>
2023-06-21 10:15:26 +0200

    Refactored Fake Loging

--- Frontend/src/pages/test/login.jsx
+++ Frontend/src/pages/test/login.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link, useParams } from 'react-router-dom'
 
 export default function TestLogin() {
   const { login_id } = useParams()
+  useEffect(
+    () =>
+      setTimeout(() => {
+        localStorage.setItem('user_id', login_id)
+      }, 100),
+    [login_id]
+  )
   return (
     <div style={{ position: 'absolute', right: '35%' }}>
       <h1> Successfully 'logged in' as User with the id {login_id}</h1>
-      {setTimeout(() => {
-        localStorage.setItem('user_id', login_id)
-      }, 100)}
       <Link to="/">Back to Home</Link>
     </div>
   )
