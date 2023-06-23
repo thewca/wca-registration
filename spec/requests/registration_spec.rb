381844361fc455ecb47079ea00883e18997d6295
FinnIckler <finnickler@gmail.com>
2023-06-23 15:55:09 +0200

    fix Rubocop errors

--- spec/requests/registration_spec.rb
+++ spec/requests/registration_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'swagger_helper'
 require_relative '../support/helpers/registration_spec_helper'
 
@@ -7,10 +9,10 @@ require_relative '../support/helpers/registration_spec_helper'
 # TODO: Refactor these into a shared example once they are passing
 RSpec.shared_context 'Registrations' do
   before do
-    basic_registration = get_registration('CubingZANationalChampionship2023-158816')
-    required_fields_only = get_registration('CubingZANationalChampionship2023-158817')
-    missing_reg_fields = get_registration('')
-    no_attendee_id = get_registration('CubingZANationalChampionship2023-158818')
+    get_registration('CubingZANationalChampionship2023-158816')
+    get_registration('CubingZANationalChampionship2023-158817')
+    get_registration('')
+    get_registration('CubingZANationalChampionship2023-158818')
   end
 end
 
@@ -26,7 +28,7 @@ end
 
 RSpec.shared_context '502 response from competition service' do
   before do
-    error_json =  { error: 'Internal Server Error for url: /api/v0/competitions/CubingZANationalChampionship2023' }.to_json
+    error_json = { error: 'Internal Server Error for url: /api/v0/competitions/CubingZANationalChampionship2023' }.to_json
 
     stub_request(:get, "https://www.worldcubeassociation.org/api/v0/competitions/#{competition_id}")
       .to_return(status: 502, body: error_json)
@@ -142,7 +144,7 @@ RSpec.describe 'v1 Registrations API', type: :request do
           end
         end
 
-  #       # TODO: Refactor to use shared_examples once passing
+        # TODO: Refactor to use shared_examples once passing
         context 'competition service not available - 502' do
           include_context '502 response from competition service'
 
@@ -155,140 +157,140 @@ RSpec.describe 'v1 Registrations API', type: :request do
           end
         end
 
-  #       # TODO: define access scopes in order to implement run this tests
+        # TODO: define access scopes in order to implement run this tests
         response '403', 'User is not allowed to access registration data (various scenarios)' do
+          puts "unimplemented"
         end
       end
 
-    post 'Create registrations in bulk' do
-      # TODO: Figure out tests for bulk registration creation endpoint
-      # NOTE: This is not currently part of our features
-    end
-  end
-
-  path '/api/v1/registration/{attendee_id}' do
-    get 'Retrieve attendee registration' do
-      parameter name: :attendee_id, in: :path, type: :string, required: true
-      produces 'application/json'
-
-      context 'success get attendee registration' do
-        existing_attendee = 'CubingZANationalChampionship2023-158816'
-
-        response '200', 'validate endpoint and schema' do
-          schema '$ref' => '#/components/schemas/registration'
-
-          let!(:attendee_id) { existing_attendee }
-
-          run_test!
-        end
-
-        response '200', 'check that registration returned matches expected registration' do
-          include_context 'Registrations' 
-
-          let!(:attendee_id) { existing_attendee }
-
-          run_test! do |response|
-            expect(response.body).to eq(basic_registration)
-          end
-        end
-      end
-
-      context 'fail get attendee registration' do
-        response '404', 'attendee_id doesnt exist' do
-          let!(:attendee_id) { 'InvalidAttendeeID' }
-
-          run_test! do |response|
-            expect(response.body).to eq({ error: "No registration found for attendee_id: #{attendee_id}." }.to_json)
-          end
-        end
+      post 'Create registrations in bulk' do
+        # TODO: Figure out tests for bulk registration creation endpoint
+        # NOTE: This is not currently part of our features
       end
     end
-  end
 
-  # TODO: POST registration tests
-  # TODO: Validate the different lanes against their schemas
-  # TODO: Figure out how to validate that webhook responses are receive? (that might be an integration/end-to-end test)
+    path '/api/v1/registration/{attendee_id}' do
+      get 'Retrieve attendee registration' do
+        parameter name: :attendee_id, in: :path, type: :string, required: true
+        produces 'application/json'
 
-  path '/api/v1/registration' do
-    post 'Add an attendee registration' do
-      parameter name: :registration, in: :body,
-                schema: { '$ref' => '#/components/schemas/registration' }, required: true
+        context 'success get attendee registration' do
+          existing_attendee = 'CubingZANationalChampionship2023-158816'
 
-      # TODO: Figure out how to validate the data written to the database
-      context 'success registration posts' do
-        response '202', 'validate schema and response' do
-          include_context 'Registrations'
-          let(:registration) { basic_registration }
+          response '200', 'validate endpoint and schema' do
+            schema '$ref' => '#/components/schemas/registration'
 
-          run_test!
-        end
+            let!(:attendee_id) { existing_attendee }
 
-        response '202', 'only required fields included' do
-          include_context 'Registrations'
-          let(:registration) { required_fields_only }
-          
-          run_test!
-        end
-      end
-
-      context 'fail: request validation fails' do
-        response 'fail', 'empty json provided' do
-          before do
-            registration = {}
+            run_test!
           end
-            
-          let!(:registration) { registration }
 
-          run_test!
+          response '200', 'check that registration returned matches expected registration' do
+            include_context 'Registrations'
+
+            let!(:attendee_id) { existing_attendee }
+
+            run_test! do |response|
+              expect(response.body).to eq(basic_registration)
+            end
+          end
         end
 
-        # TODO: Figure out how to parametrize this using shared contexts/examples once it is passing
-        response 'fail', 'not all required fields included' do
-          include_context 'Registrations'
-          
-          let!(:registration) { no_attendee_id }
+        context 'fail get attendee registration' do
+          response '404', 'attendee_id doesnt exist' do
+            let!(:attendee_id) { 'InvalidAttendeeID' }
 
-          run_test!
-        end
-
-        response 'fail', 'spelling error on field name' do
-        # TODO: write
-        end
-
-        response 'fail' 'non-permitted fields included' do
-        # TODO: write
+            run_test! do |response|
+              expect(response.body).to eq({ error: "No registration found for attendee_id: #{attendee_id}." }.to_json)
+            end
+          end
         end
       end
+    end
 
-      context 'fail: general elibigibility validation fails' do
-        response 'fail' 'attendee is banned as a competitor' do
-          # TODO: write
-          # NOTE: We need to figure out what the scope of bans are - do they prevent a user from registering at all, or only certain lanes?
-          # Have contacted WDC to confirm
+    # TODO: POST registration tests
+    # TODO: Validate the different lanes against their schemas
+    # TODO: Figure out how to validate that webhook responses are receive? (that might be an integration/end-to-end test)
+
+    path '/api/v1/registration' do
+      post 'Add an attendee registration' do
+        parameter name: :registration, in: :body,
+                  schema: { '$ref' => '#/components/schemas/registration' }, required: true
+
+        # TODO: Figure out how to validate the data written to the database
+        context 'success registration posts' do
+          response '202', 'validate schema and response' do
+            include_context 'Registrations'
+            let(:registration) { basic_registration }
+
+            run_test!
+          end
+
+          response '202', 'only required fields included' do
+            include_context 'Registrations'
+            let(:registration) { required_fields_only }
+
+            run_test!
+          end
         end
 
-        request 'fail' 'attendee has incomplete profile' do
-          # TODO: write
+        context 'fail: request validation fails' do
+          response 'fail', 'empty json provided' do
+            before do
+              {}
+            end
+
+            let!(:registration) { registration }
+
+            run_test!
+          end
+
+          # TODO: Figure out how to parametrize this using shared contexts/examples once it is passing
+          response 'fail', 'not all required fields included' do
+            include_context 'Registrations'
+
+            let!(:registration) { no_attendee_id }
+
+            run_test!
+          end
+
+          response 'fail', 'spelling error on field name' do
+            # TODO: write
+          end
+
+          response 'fail', 'non-permitted fields included' do
+            # TODO: write
+          end
         end
 
-      end
+        context 'fail: general elibigibility validation fails' do
+          response 'fail', 'attendee is banned as a competitor' do
+            # TODO: write
+            # NOTE: We need to figure out what the scope of bans are - do they prevent a user from registering at all, or only certain lanes?
+            # Have contacted WDC to confirm
+          end
 
-      context 'fail: competition elibigibility validation fails' do
+          request 'fail', 'attendee has incomplete profile' do
+            # TODO: write
+          end
+        end
+
+        context 'fail: competition elibigibility validation fails' do
           # pass
-        
-        request 'fail' 'user does not pass qualification' do
-          # TODO: write
-        end
 
-        request 'fail' 'overall attendee limit reached' do
-          # TODO: write
-          # NOTE: This would be a combination of the currently accepted attendees, those on the waiting list, and those pending
-          # NOTE: There are actually a few ways to implement this that we need to think through
+          request 'fail', 'user does not pass qualification' do
+            # TODO: write
+          end
+
+          request 'fail', 'overall attendee limit reached' do
+            # TODO: write
+            # NOTE: This would be a combination of the currently accepted attendees, those on the waiting list, and those pending
+            # NOTE: There are actually a few ways to implement this that we need to think through
+          end
         end
       end
     end
   end
 end
-
 
 # TODO: Add tests for competition_id, user_id and validity of attendee_id
