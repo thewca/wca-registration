a94c753627bb32937d3bb2e532616c7ca582dbea
Duncan <duncanonthejob@gmail.com>
2023-07-19 13:41:01 +0200

    refactored test paths

--- spec/requests/get_registrations_spec.rb
+++ spec/requests/get_registrations_spec.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 require 'swagger_helper'
-require_relative '../../support/helpers/registration_spec_helper'
-require_relative '../../../app/helpers/error_codes'
+require_relative '../support/registration_spec_helper'
+require_relative '../../app/helpers/error_codes'
 
 RSpec.describe 'v1 Registrations API', type: :request do
   include Helpers::RegistrationHelper
