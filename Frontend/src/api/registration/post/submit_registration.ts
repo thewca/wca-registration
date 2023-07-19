4671db1e249e1232a8a8596b4d32c0c8e3e16778
FinnIckler <finnickler@gmail.com>
2023-07-19 17:58:42 +0200

    run eslint

--- Frontend/src/api/registration/post/submit_registration.ts
+++ Frontend/src/api/registration/post/submit_registration.ts
@@ -1,8 +1,8 @@
 import createClient from 'openapi-fetch'
 import { getJWT } from '../../auth/get_jwt'
-import { components, paths } from '../../schema'
 import { BackendError } from '../../helper/backend_fetch'
 import { EXPIRED_TOKEN } from '../../helper/error_codes'
+import { components, paths } from '../../schema'
 
 const { post } = createClient<paths>({
   // TODO: Change this once we are fully migrated from backend fetch
