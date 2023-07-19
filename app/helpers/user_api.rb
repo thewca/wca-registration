8e0933ae7feae4a5b6c8888c105df5b0afaba70e
FinnIckler <finnickler@gmail.com>
2023-07-19 19:30:22 +0200

    removed debug statement

--- app/helpers/user_api.rb
+++ app/helpers/user_api.rb
@@ -46,7 +46,6 @@ class UserApi
     permissions = Rails.cache.fetch("#{user_id}-permissions", expires_in: 5.minutes) do
       self.get_permissions(user_id)
     end
-    puts permissions
     [permissions["can_attend_competitions"]["scope"] == "*", permissions["can_attend_competitions"]["reasons"]]
   end
 
