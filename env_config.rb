88a103fcf99299e36bcdb126090b1b9caeac7602
FinnIckler <finnickler@gmail.com>
2023-10-17 16:33:18 +0200

    make REDIS_URL optional for test

--- env_config.rb
+++ env_config.rb
@@ -10,16 +10,17 @@ EnvConfig = SuperConfig.new do
     mandatory :REGISTRATION_LIVE_SITE, :bool
     mandatory :QUEUE_URL, :string
     mandatory :WCA_HOST, :string
+    mandatory :REDIS_URL, :string
   else
     mandatory :LOCALSTACK_ENDPOINT, :string
     # Have to be the same as in localstack to simulate authentication
     mandatory :AWS_ACCESS_KEY_ID, :string
     mandatory :AWS_SECRET_ACCESS_KEY, :string
     optional :WCA_HOST, :string, ''
+    optional :REDIS_URL, :string, ''
   end
   # We even need the AWS_REGION in dev because we fake authenticate with localstack
   mandatory :AWS_REGION, :string
   mandatory :PROMETHEUS_EXPORTER, :string
-  mandatory :REDIS_URL, :string
   mandatory :DYNAMO_REGISTRATIONS_TABLE, :string
 end
