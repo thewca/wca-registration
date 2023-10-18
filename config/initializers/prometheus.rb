cdf94ca5c3af7b1e848376c8f2bb9412127568f4
FinnIckler <finnickler@gmail.com>
2023-10-18 12:07:34 +0200

    correctly use REGISTRATION_LIVE_SITE as a bool in EnvConfig

--- config/initializers/prometheus.rb
+++ config/initializers/prometheus.rb
@@ -8,7 +8,7 @@ require_relative '../../app/helpers/metrics'
 
 PrometheusExporter::Client.default = PrometheusExporter::Client.new(host: ENV.fetch('PROMETHEUS_EXPORTER'), port: 9091)
 
-if Rails.env.production? && !EnvConfig.REGISTRATION_LIVE_SITE
+if Rails.env.production? && !EnvConfig.REGISTRATION_LIVE_SITE?
   PrometheusExporter::Instrumentation::Process.start(type: 'wca-registration-handler-staging', labels: { process: '1' })
   suffix = '-staging'
 else
