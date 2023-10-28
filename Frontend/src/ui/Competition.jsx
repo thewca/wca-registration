86e8f3846dbaf3fb4f1afc060922be46017cbc27
Finn Ickler <finnickler@gmail.com>
2023-10-27 22:26:43 -0400

    Move Competition Box to Semantic

--- Frontend/src/ui/Competition.jsx
+++ Frontend/src/ui/Competition.jsx
@@ -5,7 +5,15 @@ import { dinero, toDecimal } from 'dinero.js'
 import moment from 'moment'
 import React from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
-import { Button, Image } from 'semantic-ui-react'
+import {
+  Button,
+  Container,
+  Grid,
+  Header,
+  Image,
+  Label,
+  Segment,
+} from 'semantic-ui-react'
 import getCompetitionInfo from '../api/competition/get/get_competition_info'
 import { CompetitionContext } from '../api/helper/context/competition_context'
 import { BASE_ROUTE } from '../routes'
@@ -27,70 +35,80 @@ export default function Competition({ children }) {
         <LoadingMessage />
       ) : (
         <>
-          <div className={styles.competitionInfo}>
-            <div className={styles.header}>
-              <div className={styles.infoLeft}>
-                <div className={styles.competitionName}>
-                  <UiIcon name="bookmark ouline" /> {competitionInfo.name} |{' '}
-                  {competitionInfo['registration_opened?'] ? (
-                    <span className={styles.open}>Open</span>
-                  ) : (
-                    <span className={styles.close}>Close</span>
-                  )}
-                </div>
-                <div className={styles.location}>
-                  <UiIcon name="pin" /> {competitionInfo.venue_address}
-                </div>
-                <div className={styles.date}>
-                  {moment(competitionInfo.start_date).format('LL')},{' '}
-                  <a
-                    href={`https://calendar.google.com/calendar/render?action=TEMPLATE&text=${
-                      competitionInfo.id
-                    }&dates=${moment(competitionInfo.start_date).format(
-                      'YYYYMMDD'
-                    )}/${moment(competitionInfo.end_date).format(
-                      'YYYYMMDD'
-                    )}&location=${competitionInfo.venue_address}`}
-                  >
-                    Add to Google Calendar
-                  </a>
-                </div>
-                <div className={styles.announcement}>
-                  *Insert Potential organizer announcement or memo for users to
-                  view before hitting register*
-                </div>
-                <Button
-                  className={styles.registerButton}
-                  disabled={!competitionInfo['registration_opened?']}
-                  onClick={(_, data) => {
-                    if (!data.disabled) {
-                      if (competitionInfo.use_wca_registration) {
-                        navigate(`${BASE_ROUTE}/${competitionInfo.id}/register`)
-                      } else {
-                        window.location =
-                          competitionInfo.external_registration_page
+          <Container>
+            <Segment padded>
+              <Grid>
+                <Grid.Column width={12}>
+                  <Header as="h1">
+                    <UiIcon name="bookmark ouline" />
+                    {competitionInfo['registration_opened?'] ? (
+                      <Header.Content>
+                        {competitionInfo.name} | Open
+                      </Header.Content>
+                    ) : (
+                      <Header.Content>
+                        {competitionInfo.name} | Close
+                      </Header.Content>
+                    )}
+                    <Header.Subheader>
+                      <UiIcon name="pin" /> {competitionInfo.venue_address}
+                    </Header.Subheader>
+                  </Header>
+                  <Header as="h2">
+                    {moment(competitionInfo.start_date).format('LL')},{' '}
+                    <a
+                      href={`https://calendar.google.com/calendar/render?action=TEMPLATE&text=${
+                        competitionInfo.id
+                      }&dates=${moment(competitionInfo.start_date).format(
+                        'YYYYMMDD'
+                      )}/${moment(competitionInfo.end_date).format(
+                        'YYYYMMDD'
+                      )}&location=${competitionInfo.venue_address}`}
+                    >
+                      Add to Google Calendar
+                    </a>
+                  </Header>
+                  <Segment inverted color="red">
+                    *Insert Potential organizer announcement or memo for users
+                    to view before hitting register*
+                  </Segment>
+
+                  <Button
+                    color="orange"
+                    size="massive"
+                    disabled={!competitionInfo['registration_opened?']}
+                    onClick={(_, data) => {
+                      if (!data.disabled) {
+                        if (competitionInfo.use_wca_registration) {
+                          navigate(
+                            `${BASE_ROUTE}/${competitionInfo.id}/register`
+                          )
+                        } else {
+                          window.location =
+                            competitionInfo.external_registration_page
+                        }
                       }
-                    }
-                  }}
-                >
-                  Register
-                </Button>
-                <span className={styles.fee}>
-                  Registration Fee:{' '}
-                  {toDecimal(
-                    dinero({
-                      amount:
-                        competitionInfo.base_entry_fee_lowest_denomination,
-                      currency: currencies[competitionInfo.currency_code],
-                    }),
-                    ({ value, currency }) => `${currency.code} ${value}`
-                  ) ?? 'No Entry Fee'}
-                </span>
-              </div>
-              <div className={styles.infoRight}>
-                <Image href={competitionInfo.url} className={styles.image} />
-              </div>
-            </div>
+                    }}
+                  >
+                    Register
+                  </Button>
+                  <Label size="massive">
+                    Registration Fee:{' '}
+                    {toDecimal(
+                      dinero({
+                        amount:
+                          competitionInfo.base_entry_fee_lowest_denomination,
+                        currency: currencies[competitionInfo.currency_code],
+                      }),
+                      ({ value, currency }) => `${currency.code} ${value}`
+                    ) ?? 'No Entry Fee'}
+                  </Label>
+                </Grid.Column>
+                <Grid.Column width={4}>
+                  <Image href={competitionInfo.url} className={styles.image} />
+                </Grid.Column>
+              </Grid>
+            </Segment>
             <div className={styles.eventList}>
               <div>
                 <span className={styles.eventHeader}>Events:</span>
@@ -110,7 +128,7 @@ export default function Competition({ children }) {
                 </span>
               </div>
             </div>
-          </div>
+          </Container>
           {children}
         </>
       )}
