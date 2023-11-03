2fe9cd490af2f5fa1979592e6421c0eb863cffa5
Finn Ickler <finnickler@gmail.com>
2023-11-03 11:09:03 -0400

    add import route to index

--- Frontend/src/pages/import/index.jsx
+++ Frontend/src/pages/import/index.jsx
@@ -1,7 +1,8 @@
 import { useMutation } from '@tanstack/react-query'
-import { useContext, useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Button, Input, Segment } from 'semantic-ui-react'
+import { CompetitionContext } from '../../api/helper/context/competition_context'
 import { PermissionsContext } from '../../api/helper/context/permission_context'
 import importRegistration from '../../api/registration/post/import_registration'
 import { BASE_ROUTE } from '../../routes'
@@ -10,7 +11,7 @@ import PermissionMessage from '../../ui/messages/permissionMessage'
 export default function Import() {
   const [file, setFile] = useState()
   const navigate = useNavigate()
-  const { competitionInfo } = useContext(competitionInfo)
+  const { competitionInfo } = useContext(CompetitionContext)
   const { canAdminCompetition } = useContext(PermissionsContext)
   const { mutate: importMutation, isLoading: isMutating } = useMutation({
     mutationFn: importRegistration,
