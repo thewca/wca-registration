5d78affa8247df2b2f76b8fef47490ff4379d095
Duncan <duncanonthejob@gmail.com>
2023-11-09 18:45:37 +0200

    removed old validation logic from controller

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -49,30 +49,10 @@ class RegistrationController < ApplicationController
     render json: { status: 'accepted', message: 'Started Registration Process' }, status: :accepted
   end
 
-  # For a user to register they need to
-  # 1) Need to actually be the user that they are trying to register
-  # 2) Be Eligible to Compete (complete profile + not banned)
-  # 3) Register for a competition that is open
-  # 4) Register for events that are actually held at the competition
-  # We need to do this in this order, so we don't leak user attributes
   def validate_create_request
-    @user_id = registration_params[:user_id]
     @competition_id = registration_params[:competition_id]
-    @event_ids = registration_params[:competing]['event_ids']
 
-    # TODO: Rename @comeptition to competition_info - make it clear that it's a DataClass, not a model object
-    @competition = CompetitionApi.find!(@competition_id)
-
-    puts "current user: #{@current_user}"
     RegistrationChecker.create_registration_allowed!(registration_params, CompetitionApi.find!(@competition_id), @current_user)
-
-    # user_can_create_registration!
-
-    # can_compete, reasons = UserApi.can_compete?(@user_id)
-    # raise RegistrationError.new(:unauthorized, reasons) unless can_compete
-
-    # validate_events!
-    # raise RegistrationError.new(:unprocessable_entity, ErrorCodes::GUEST_LIMIT_EXCEEDED) if params.key?(:guests) && @competition.guest_limit_exceeded?(params[:guests])
   rescue RegistrationError => e
     render_error(e.http_status, e.error)
   end
@@ -135,23 +115,10 @@ class RegistrationController < ApplicationController
 
   # You can either update your own registration or one for a competition you administer
   def validate_update_request
-    puts "update params: #{params}"
     @user_id = params[:user_id]
     @competition_id = params[:competition_id]
 
-    # @registration = Registration.find("#{@competition_id}-#{@user_id}")
-
     RegistrationChecker.update_registration_allowed!(params, CompetitionApi.find!(@competition_id), @current_user)
-    # raise RegistrationError.new(:unauthorized, ErrorCodes::USER_INSUFFICIENT_PERMISSIONS) unless is_admin_or_current_user?
-    # raise RegistrationError.new(:unauthorized, ErrorCodes::USER_INSUFFICIENT_PERMISSIONS) if admin_fields_present? && !UserApi.can_administer?(@current_user, @competition_id)
-    # raise RegistrationError.new(:unprocessable_entity, ErrorCodes::GUEST_LIMIT_EXCEEDED) if params.key?(:guests) && @competition.guest_limit_exceeded?(params[:guests])
-
-    # if params.key?(:competing)
-    #   validate_status! if params['competing'].key?(:status)
-    #   validate_events! if params['competing'].key?(:event_ids)
-    #   raise RegistrationError.new(:unprocessable_entity, ErrorCodes::USER_COMMENT_TOO_LONG) if params['competing'].key?(:comment) && !comment_valid?
-    #   raise RegistrationError.new(:unprocessable_entity, ErrorCodes::REQUIRED_COMMENT_MISSING) if !params['competing'].key?(:comment) && @competition.force_comment?
-    # end
   rescue Dynamoid::Errors::RecordNotFound
     render_error(:not_found, ErrorCodes::REGISTRATION_NOT_FOUND)
   rescue RegistrationError => e
@@ -290,70 +257,5 @@ class RegistrationController < ApplicationController
           updated_at: registration.payment_date,
         },
       }
-    end
-
-    def registration_exists?(user_id, competition_id)
-      Registration.find("#{competition_id}-#{user_id}")
-      true
-    rescue Dynamoid::Errors::RecordNotFound
-      false
-    end
-
-    def comment_valid?
-      params['competing'][:comment].length <= 240
-    end
-
-    def validate_events!
-      event_ids = params['competing'][:event_ids]
-      if defined?(@registration) && params['competing'].key?(:status) && params['competing'][:status] == 'cancelled'
-        # If status is cancelled, events can only be empty or match the old events list
-        # This allows for edge cases where an API user might send an empty event list/the old event list, or admin might want to remove events
-        raise RegistrationError.new(:unprocessable_entity, ErrorCodes::INVALID_EVENT_SELECTION) unless event_ids == [] || event_ids == @registration.event_ids
-      else
-        # Event submitted must be held at the competition
-        raise RegistrationError.new(:unprocessable_entity, ErrorCodes::INVALID_EVENT_SELECTION) unless @competition.events_held?(event_ids)
-      end
-
-      # Events can't be changed outside the edit_events deadline
-      # TODO: Should an admin be able to override this?
-      if @competition.event_change_deadline.present?
-        events_edit_deadline = Time.parse(@competition.event_change_deadline)
-        raise RegistrationError.new(:forbidden, ErrorCodes::EVENT_EDIT_DEADLINE_PASSED) if events_edit_deadline < Time.now
-      end
-    end
-
-    def admin_fields_present?
-      # There could be different admin fields in different lanes - define the admin fields per lane and check each
-      competing_admin_fields = ['admin_comment']
-
-      params.key?('competing') && params['competing'].keys.any? { |key| competing_admin_fields.include?(key) }
-    end
-
-    def user_can_create_registration!
-      # Only an admin or the user themselves can create a registration for the user
-      raise RegistrationError.new(:unauthorized, ErrorCodes::USER_INSUFFICIENT_PERMISSIONS) unless is_admin_or_current_user?
-
-      # Only admins can register when registration is closed, and they can only register for themselves - not for other users
-      raise RegistrationError.new(:forbidden, ErrorCodes::REGISTRATION_CLOSED) unless @competition.registration_open? || organizer_signing_up_themselves?
-    end
-
-    def organizer_signing_up_themselves?
-      @competition.is_organizer_or_delegate?(@current_user) && (@current_user == @user_id.to_s)
-    end
-
-    def is_admin_or_current_user?
-      # Only an admin or the user themselves can create a registration for the user
-      # One case where admins need to create registrations for users is if a 3rd-party registration system is being used, and registration data is being
-      # passed to the Registration Service from it
-      (@current_user == @user_id.to_s) || UserApi.can_administer?(@current_user, @competition_id)
-    end
-
-    def validate_status!
-      raise RegistrationError.new(:unprocessable_entity, ErrorCodes::INVALID_REQUEST_DATA) unless Registration::REGISTRATION_STATES.include?(params['competing'][:status])
-
-      raise RegistrationError.new(:unauthorized, ErrorCodes::USER_INSUFFICIENT_PERMISSIONS) if
-        Registration::ADMIN_ONLY_STATES.include?(params['competing'][:status]) && !UserApi.can_administer?(@current_user, @competition_id)
-
-      raise RegistrationError.new(:forbidden, ErrorCodes::COMPETITOR_LIMIT_REACHED) if params['competing'][:status] == 'accepted' && Registration.count > @competition.competitor_limit
     end
 end
