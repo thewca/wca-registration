fc22684028a681ab5d5a929a7a506297649be465
Duncan <duncanonthejob@gmail.com>
2023-11-17 12:37:39 +0200

    removed fetch_jwt_token def in request_factory

--- spec/factories/request_factory.rb
+++ spec/factories/request_factory.rb
@@ -3,16 +3,6 @@
 require 'factory_bot_rails'
 require_relative '../support/jwt_token_generator'
 
-# Couldn't get the import from a support folder to work, so defining directly in the factory file
-# def fetch_jwt_token(user_id)
-#   iat = Time.now.to_i
-#   jti_raw = [JwtOptions.secret, iat].join(':').to_s
-#   jti = Digest::MD5.hexdigest(jti_raw)
-#   payload = { user_id: user_id, exp: Time.now.to_i + JwtOptions.expiry, sub: user_id, iat: iat, jti: jti }
-#   token = JWT.encode payload, JwtOptions.secret, JwtOptions.algorithm
-#   "Bearer #{token}"
-# end
-
 FactoryBot.define do
   factory :registration_request, class: Hash do
     transient do
