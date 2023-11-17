a9a81a1eed5f5424a01416a7c798e68af8bb1ab1
Duncan <duncanonthejob@gmail.com>
2023-11-17 13:06:56 +0200

    skipping jwt token validation on reset route

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -4,6 +4,9 @@ require_relative '../../spec/support/dynamoid_reset'
 
 class TestController < ApplicationController
   include DynamoidReset
+
+  skip_before_action :validate_token, only: [:reset]
+
   def reset
     return head :forbidden if Rails.env.production?
     DynamoidReset.all
--- spec/controllers/test_controller_spec.rb
+++ spec/controllers/test_controller_spec.rb
@@ -1,24 +1,19 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-require_relative '../support/jwt_token_generator'
 
 describe TestController do
   describe '#test_reset.test_env' do
     it 'route is available when RAILS_ENV is test' do
-      jwt_token = fetch_jwt_token('1306')
-      request.headers['Authorization'] = jwt_token
       get :reset
       expect(response).to have_http_status(200)
     end
 
     it 'resets the database when called' do
-      jwt_token = fetch_jwt_token('1306')
       FactoryBot.create_list(:registration, 3, registration_status: 'accepted')
       expect(Registration.all.count).to eq(3)
 
-      request.headers['Authorization'] = jwt_token
-      post :reset
+      get :reset
       expect(Registration.all.count).to eq(0)
     end
   end
@@ -35,9 +30,7 @@ describe TestController do
     end
 
     it 'controller rejects request when RAILS_ENV is production' do
-      jwt_token = fetch_jwt_token('1306')
-      request.headers['Authorization'] = jwt_token
-      post :reset
+      get :reset
 
       expect(response).to have_http_status(403)
     end
