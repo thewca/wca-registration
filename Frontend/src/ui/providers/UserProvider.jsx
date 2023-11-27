0126cd1c00f58f0b837b45a874af7d4cc5ff0445
Finn Ickler <finnickler@gmail.com>
2023-11-27 10:23:47 +0100

    make sure to regenerate JWT on relogin

--- Frontend/src/ui/providers/UserProvider.jsx
+++ Frontend/src/ui/providers/UserProvider.jsx
@@ -1,5 +1,6 @@
 import { useQuery } from '@tanstack/react-query'
 import React from 'react'
+import { getJWT } from '../../api/auth/get_jwt'
 import { UserContext } from '../../api/helper/context/user_context'
 import getMe from '../../api/user/get/get_me'
 import LoadingMessage from '../messages/loadingMessage'
@@ -14,6 +15,10 @@ export default function UserProvider({ children }) {
     retry: false,
     onError: (_) => {
       // We are not logged in, set user explicitly to undefined?
+    },
+    onSuccess: (_) => {
+      // Get a new token so when a user switches account it overwrites the token
+      getJWT(true)
     },
   })
   return isLoading ? (
