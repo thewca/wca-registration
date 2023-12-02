d9a855c4256f72691d4d8e707567f668e1ea9204
Kevin Matthews <kvnrmatthews@gmail.com>
2023-12-01 16:06:26 -0800

    Separate information grid into own component
    and minor formatting

--- Frontend/src/pages/home/index.jsx
+++ Frontend/src/pages/home/index.jsx
@@ -8,12 +8,14 @@ import RegistrationRequirements from '../register/components/RegistrationRequire
 import styles from './index.module.scss'
 
 export default function HomePage() {
-  const { competitionInfo } = useContext(CompetitionContext)
+  const { competitionInfo } = useContext(CompetitionContext);
+  
   return (
     <Container>
       <div>
         <RegistrationRequirements />
       </div>
+
       {competitionInfo.information && (
         <div>
           <Header as="h3">Information:</Header>
@@ -25,6 +27,7 @@ export default function HomePage() {
           />
         </div>
       )}
+
       <Header as="h3">
         Registration Period:
         <Header.Subheader>
@@ -39,95 +42,10 @@ export default function HomePage() {
               ).calendar()}`}
         </Header.Subheader>
       </Header>
+
       <Segment padded attached>
-        <Grid>
-          <Grid.Column width={3}>
-            <Header className={styles.informationHeader}>Date</Header>
-            <Header className={styles.informationHeader}>City</Header>
-            <Header className={styles.informationHeader}>Venue</Header>
-            <Header className={styles.informationHeader} color="grey">
-              Address
-            </Header>
-            {competitionInfo.venue_details && (
-              <Header className={styles.informationHeader} color="grey">
-                Details
-              </Header>
-            )}
-            <Header className={styles.informationHeader}>
-              Competitor Limit
-            </Header>
-            <Header className={styles.informationHeader}>Contact</Header>
-            <Header className={styles.informationHeader}>Organizers</Header>
-            <Header className={styles.informationHeader}>Delegates</Header>
-          </Grid.Column>
-          <Grid.Column width={12}>
-            <Header className={styles.informationHeader}>
-              {competitionInfo.start_date === competitionInfo.end_date
-                ? `${moment(competitionInfo.start_date).format('ll')}`
-                : `${moment(competitionInfo.start_date).format(
-                    'll'
-                  )} to ${moment(competitionInfo.end_date).format('ll')}`}
-            </Header>
-            <Header className={styles.informationHeader}>
-              {competitionInfo.city}, {competitionInfo.country_iso2}
-            </Header>
-            <Header className={styles.informationHeader}>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: marked(competitionInfo.venue),
-                }}
-              />
-            </Header>
-            <Header className={styles.informationHeader} color="grey">
-              {competitionInfo.venue_address}
-            </Header>
-            {competitionInfo.venue_details && (
-              <Header className={styles.informationHeader} color="grey">
-                {competitionInfo.venue_details}
-              </Header>
-            )}
-            <Header className={styles.informationHeader}>
-              {competitionInfo.competitor_limit}
-            </Header>
-            <Header className={styles.informationHeader}>
-              {competitionInfo.contact ? (
-                <span
-                  dangerouslySetInnerHTML={{
-                    __html: marked(competitionInfo.contact),
-                  }}
-                />
-              ) : (
-                <a
-                  href={`https://www.worldcubeassociation.org/contact/website?competitionId=${competitionInfo.id}`}
-                >
-                  Organization Team
-                </a>
-              )}
-            </Header>
-            <Header className={styles.informationHeader}>
-              {competitionInfo.organizers.map((organizer, index) => (
-                <a
-                  key={`competition-organizer-${organizer.id}`}
-                  href={`${process.env.WCA_URL}/persons/${organizer.wca_id}`}
-                >
-                  {organizer.name}
-                  {index !== competitionInfo.organizers.length - 1 ? ', ' : ''}
-                </a>
-              ))}
-            </Header>
-            <Header className={styles.informationHeader}>
-              {competitionInfo.delegates.map((delegate, index) => (
-                <a
-                  key={`competition-organizer-${delegate.id}`}
-                  href={`${process.env.WCA_URL}/persons/${delegate.wca_id}`}
-                >
-                  {delegate.name}
-                  {index !== competitionInfo.delegates.length - 1 ? ', ' : ''}
-                </a>
-              ))}
-            </Header>
-          </Grid.Column>
-        </Grid>
+        <InformationGrid competitionInfo={competitionInfo} />
+
         <Header className={styles.informationHeader}>
           <UiIcon name="print" />
           <Header.Content>
@@ -140,10 +58,103 @@ export default function HomePage() {
           </Header.Content>
         </Header>
       </Segment>
+
       <Header attached="bottom" textAlign="center" as="h2">
         The Competition has been bookmarked{' '}
         {competitionInfo.number_of_bookmarks} times
       </Header>
     </Container>
+  )
+}
+
+function InformationGrid({ competitionInfo }) {
+  return (<Grid>
+    <Grid.Column width={3}>
+      <Header className={styles.informationHeader}>Date</Header>
+      <Header className={styles.informationHeader}>City</Header>
+      <Header className={styles.informationHeader}>Venue</Header>
+      <Header className={styles.informationHeader} color="grey">
+        Address
+      </Header>
+      {competitionInfo.venue_details && (
+        <Header className={styles.informationHeader} color="grey">
+          Details
+        </Header>
+      )}
+      <Header className={styles.informationHeader}>
+        Competitor Limit
+      </Header>
+      <Header className={styles.informationHeader}>Contact</Header>
+      <Header className={styles.informationHeader}>Organizers</Header>
+      <Header className={styles.informationHeader}>Delegates</Header>
+    </Grid.Column>
+    <Grid.Column width={12}>
+      <Header className={styles.informationHeader}>
+        {competitionInfo.start_date === competitionInfo.end_date
+          ? `${moment(competitionInfo.start_date).format('ll')}`
+          : `${moment(competitionInfo.start_date).format(
+              'll'
+            )} to ${moment(competitionInfo.end_date).format('ll')}`}
+      </Header>
+      <Header className={styles.informationHeader}>
+        {competitionInfo.city}, {competitionInfo.country_iso2}
+      </Header>
+      <Header className={styles.informationHeader}>
+        <p
+          dangerouslySetInnerHTML={{
+            __html: marked(competitionInfo.venue),
+          }}
+        />
+      </Header>
+      <Header className={styles.informationHeader} color="grey">
+        {competitionInfo.venue_address}
+      </Header>
+      {competitionInfo.venue_details && (
+        <Header className={styles.informationHeader} color="grey">
+          {competitionInfo.venue_details}
+        </Header>
+      )}
+      <Header className={styles.informationHeader}>
+        {competitionInfo.competitor_limit}
+      </Header>
+      <Header className={styles.informationHeader}>
+        {competitionInfo.contact ? (
+          <span
+            dangerouslySetInnerHTML={{
+              __html: marked(competitionInfo.contact),
+            }}
+          />
+        ) : (
+          <a
+            href={`https://www.worldcubeassociation.org/contact/website?competitionId=${competitionInfo.id}`}
+          >
+            Organization Team
+          </a>
+        )}
+      </Header>
+      <Header className={styles.informationHeader}>
+        {competitionInfo.organizers.map((organizer, index) => (
+          <a
+            key={`competition-organizer-${organizer.id}`}
+            href={`${process.env.WCA_URL}/persons/${organizer.wca_id}`}
+          >
+            {organizer.name}
+            {index !== competitionInfo.organizers.length - 1 ? ', ' : ''}
+          </a>
+        ))}
+      </Header>
+      <Header className={styles.informationHeader}>
+        {competitionInfo.delegates.map((delegate, index) => (
+          <a
+            key={`competition-organizer-${delegate.id}`}
+            href={`${process.env.WCA_URL}/persons/${delegate.wca_id}`}
+          >
+            {delegate.name}
+            {index !== competitionInfo.delegates.length - 1 ? ', ' : ''}
+          </a>
+        ))}
+      </Header>
+    </Grid.Column>
+  </Grid>
   )
 }
