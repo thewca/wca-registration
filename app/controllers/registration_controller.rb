07a5f349b4440df2543e51138d1ede5f39e8583a
Finn Ickler <finnickler@gmail.com>
2023-12-07 19:02:18 +0100

    run rubocop

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -95,9 +95,9 @@ class RegistrationController < ApplicationController
       registration = Registration.find("#{@competition_id}-#{@user_id}")
       old_status = registration.competing_status
       updated_registration = registration.update_competing_lane!({ status: status, comment: comment, event_ids: event_ids, admin_comment: admin_comment, guests: guests })
-      if old_status == "accepted" && status != "accepted"
+      if old_status == 'accepted' && status != 'accepted'
         Registration.decrement_competitors_count(@competition_id)
-      elsif old_status != "accepted" && status == "accepted"
+      elsif old_status != 'accepted' && status == 'accepted'
         Registration.increment_competitors_count(@competition_id)
       end
       render json: { status: 'ok', registration: {
--- app/lib/redis.rb
+++ app/lib/redis.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Redis
   def increment_or_initialize(key, &block)
     if Rails.cache.exist?(key)
