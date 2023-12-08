65e82142c224f7c10899d7ee6679f29d2b91a203
Kevin Matthews <kvnrmatthews@gmail.com>
2023-12-07 17:10:11 -0800

    Move registration steps below registration info
    makes for much less jumping around

--- Frontend/src/pages/register/index.jsx
+++ Frontend/src/pages/register/index.jsx
@@ -43,23 +43,6 @@ export default function Register() {
         <div>
           {canAttendCompetition ? (
             <>
-              <Transition
-                visible={showRegisterSteps}
-                duration={500}
-                animation="scale"
-              >
-                <Segment padded basic>
-                  <Button
-                    floated="right"
-                    icon
-                    basic
-                    onClick={() => setShowRegisterSteps(false)}
-                  >
-                    <Icon name="close" />
-                  </Button>
-                  <StepPanel />
-                </Segment>
-              </Transition>
               <Segment padded attached raised>
                 <Message warning>
                   *Insert Potential organizer announcement or memo for users to
@@ -206,6 +189,24 @@ export default function Register() {
                   </Button>
                 </Transition>
               </Segment>
+
+              <Transition
+                visible={showRegisterSteps}
+                duration={500}
+                animation="scale"
+              >
+                <Segment padded basic>
+                  <Button
+                    floated="right"
+                    icon
+                    basic
+                    onClick={() => setShowRegisterSteps(false)}
+                  >
+                    <Icon name="close" />
+                  </Button>
+                  <StepPanel />
+                </Segment>
+              </Transition>
             </>
           ) : (
             <PermissionMessage>
