a87e96f209d90b2520911d826e5682c16bb978b1
Kevin Matthews <49137025+kr-matthews@users.noreply.github.com>
2023-12-11 09:08:11 -0500

    Hide page tabs 'More' when empty & move registration steps down (#383)
    * Hide 'More' menu item when empty
    
    * Move constants outside of PageTabs component
    
    * Move registration steps below registration info
    
    makes for much less jumping around

--- Frontend/src/ui/PageTabs.jsx
+++ Frontend/src/ui/PageTabs.jsx
@@ -6,6 +6,103 @@ import { CompetitionContext } from '../api/helper/context/competition_context'
 import { PermissionsContext } from '../api/helper/context/permission_context'
 import { BASE_ROUTE } from '../routes'
 
+export default function PageTabs() {
+  const { competitionInfo } = useContext(CompetitionContext)
+  const { canAdminCompetition } = useContext(PermissionsContext)
+
+  const navigate = useNavigate()
+  const location = useLocation()
+
+  const menuItems = useMemo(() => {
+    const optionalTabs = []
+
+    if (competitionInfo.use_wca_registration) {
+      optionalTabs.push(registerMenuConfig)
+    }
+    if (canAdminCompetition) {
+      optionalTabs.push(registrationsMenuConfig)
+    }
+    if (new Date(competitionInfo.registration_open) < Date.now()) {
+      optionalTabs.push(competitorsMenuConfig)
+    }
+
+    const eventTabIcon =
+      competitionInfo.main_event_id ?? competitionInfo.event_ids[0]
+
+    return [
+      generalInfoMenuConfig,
+      ...optionalTabs,
+      eventsMenuConfig(eventTabIcon),
+      scheduleMenuConfig,
+    ]
+  }, [
+    competitionInfo.use_wca_registration,
+    competitionInfo.registration_open,
+    competitionInfo.main_event_id,
+    competitionInfo.event_ids,
+    canAdminCompetition,
+  ])
+
+  const customTabActive = useMemo(() => {
+    return competitionInfo.tabs.some((competitionTab) =>
+      pathMatch(competitionTab.id, location.pathname)
+    )
+  }, [competitionInfo.tabs, location])
+
+  const hasCustomTabs = competitionInfo.tabs.length > 0
+
+  return (
+    <Menu
+      attached
+      fluid
+      widths={menuItems.length + (hasCustomTabs ? 1 : 0)}
+      size="huge"
+      stackable
+    >
+      {menuItems.map((menuConfig) => (
+        <Menu.Item
+          key={menuConfig.key}
+          name={menuConfig.key}
+          onClick={() =>
+            navigate(
+              `${BASE_ROUTE}/${competitionInfo.id}/${
+                menuConfig.route ?? menuConfig.key
+              }`
+            )
+          }
+          active={pathMatch(menuConfig.key, location.pathname)}
+        >
+          {menuConfig.cubing && <CubingIcon event={menuConfig.icon} selected />}
+          {menuConfig.icon && !menuConfig.cubing && (
+            <UiIcon name={menuConfig.icon} />
+          )}
+          {menuConfig.label}
+        </Menu.Item>
+      ))}
+
+      {hasCustomTabs && (
+        <Dropdown item text="More" className={customTabActive ? 'active' : ''}>
+          <Dropdown.Menu>
+            {competitionInfo.tabs.map((competitionTab) => (
+              <Dropdown.Item
+                key={competitionTab.id}
+                onClick={() =>
+                  navigate(
+                    `${BASE_ROUTE}/${competitionInfo.id}/tabs/${competitionTab.id}`
+                  )
+                }
+                active={pathMatch(competitionTab.id, location.pathname)}
+              >
+                {competitionTab.name}
+              </Dropdown.Item>
+            ))}
+          </Dropdown.Menu>
+        </Dropdown>
+      )}
+    </Menu>
+  )
+}
+
 function pathMatch(name, pathname) {
   const registerExpression = /\/competitions\/v2\/[a-zA-Z0-9]+\/register/
   const registrationsExpression =
@@ -35,113 +132,37 @@ function pathMatch(name, pathname) {
   }
 }
 
-export default function PageTabs() {
-  const { competitionInfo } = useContext(CompetitionContext)
-  const { canAdminCompetition } = useContext(PermissionsContext)
-
-  const navigate = useNavigate()
-  const location = useLocation()
-
-  const menuItems = useMemo(() => {
-    const optionalTabs = []
-
-    if (competitionInfo.use_wca_registration) {
-      optionalTabs.push({
-        key: 'register',
-        icon: 'sign in alt',
-        label: 'Register',
-      })
-    }
-    if (canAdminCompetition) {
-      optionalTabs.push({
-        key: 'registrations',
-        route: 'registrations/edit',
-        icon: 'list ul',
-        label: 'Registrations',
-      })
-    }
-    if (new Date(competitionInfo.registration_open) < Date.now()) {
-      optionalTabs.push({
-        key: 'competitors',
-        route: 'registrations',
-        icon: 'users',
-        label: 'Competitors',
-      })
-    }
-
-    return [
-      {
-        key: 'info',
-        route: '',
-        icon: 'info',
-        label: 'General Info',
-      },
-      ...optionalTabs,
-      {
-        key: 'events',
-        icon: competitionInfo.main_event_id ?? competitionInfo.event_ids[0],
-        label: 'Events',
-        cubing: true,
-      },
-      {
-        key: 'schedule',
-        icon: 'calendar',
-        label: 'Schedule',
-      },
-    ]
-  }, [
-    competitionInfo.use_wca_registration,
-    competitionInfo.registration_open,
-    competitionInfo.main_event_id,
-    competitionInfo.event_ids,
-    canAdminCompetition,
-  ])
-
-  const customTabActive = useMemo(() => {
-    return competitionInfo.tabs.some((competitionTab) =>
-      pathMatch(competitionTab.id, location.pathname)
-    )
-  }, [competitionInfo.tabs, location])
-
-  return (
-    <Menu attached fluid widths={menuItems.length + 1} size="huge" stackable>
-      {menuItems.map((menuConfig) => (
-        <Menu.Item
-          key={menuConfig.key}
-          name={menuConfig.key}
-          onClick={() =>
-            navigate(
-              `${BASE_ROUTE}/${competitionInfo.id}/${
-                menuConfig.route ?? menuConfig.key
-              }`
-            )
-          }
-          active={pathMatch(menuConfig.key, location.pathname)}
-        >
-          {menuConfig.cubing && <CubingIcon event={menuConfig.icon} selected />}
-          {menuConfig.icon && !menuConfig.cubing && (
-            <UiIcon name={menuConfig.icon} />
-          )}
-          {menuConfig.label}
-        </Menu.Item>
-      ))}
-      <Dropdown item text="More" className={customTabActive ? 'active' : ''}>
-        <Dropdown.Menu>
-          {competitionInfo.tabs.map((competitionTab) => (
-            <Dropdown.Item
-              key={competitionTab.id}
-              onClick={() =>
-                navigate(
-                  `${BASE_ROUTE}/${competitionInfo.id}/tabs/${competitionTab.id}`
-                )
-              }
-              active={pathMatch(competitionTab.id, location.pathname)}
-            >
-              {competitionTab.name}
-            </Dropdown.Item>
-          ))}
-        </Dropdown.Menu>
-      </Dropdown>
-    </Menu>
-  )
+const registerMenuConfig = {
+  key: 'register',
+  icon: 'sign in alt',
+  label: 'Register',
+}
+const registrationsMenuConfig = {
+  key: 'registrations',
+  route: 'registrations/edit',
+  icon: 'list ul',
+  label: 'Registrations',
+}
+const competitorsMenuConfig = {
+  key: 'competitors',
+  route: 'registrations',
+  icon: 'users',
+  label: 'Competitors',
+}
+const generalInfoMenuConfig = {
+  key: 'info',
+  route: '',
+  icon: 'info',
+  label: 'General Info',
+}
+const eventsMenuConfig = (icon) => ({
+  key: 'events',
+  icon,
+  label: 'Events',
+  cubing: true,
+})
+const scheduleMenuConfig = {
+  key: 'schedule',
+  icon: 'calendar',
+  label: 'Schedule',
 }
