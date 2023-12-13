b1bdeb8ed2caa7b6778b71a4c3047a6fb5aee960
FinnIckler <finnickler@gmail.com>
2023-12-13 21:38:33 +0100

    fix issue with multiple updates not waiting before the other completes

--- Frontend/src/pages/registration_administration/components/RegistrationActions.jsx
+++ Frontend/src/pages/registration_administration/components/RegistrationActions.jsx
@@ -62,9 +62,9 @@ export default function RegistrationActions({
       )
     },
   })
-  const changeStatus = async (attendees, status) => {
-    attendees.forEach((attendee) => {
-      updateRegistrationMutation(
+  const changeStatus = (attendees, status) => {
+    attendees.forEach(async (attendee) => {
+      await updateRegistrationMutation(
         {
           user_id: attendee,
           competing: {
