a656cbf09fa46c3910cb84396ca773df7809f489
Kevin Matthews <kvnrmatthews@gmail.com>
2023-12-16 17:26:56 -0800

    Update view selector UI to secondary menu

--- Frontend/src/pages/schedule/ViewSelector.jsx
+++ Frontend/src/pages/schedule/ViewSelector.jsx
@@ -1,29 +1,19 @@
 import React from 'react'
-import { Checkbox, Form } from 'semantic-ui-react'
+import { Menu } from 'semantic-ui-react'
+
+const views = ['calendar', 'table']
 
 export default function ViewSelector({ activeView, setActiveView }) {
   return (
-    <Form>
-      <Form.Field>
-        <Checkbox
-          radio
-          label="Calendar View"
-          name="viewGroup"
-          value="calendar"
-          checked={activeView === 'calendar'}
-          onChange={(_, data) => setActiveView(data.value)}
+    <Menu pointing secondary fluid widths={2}>
+      {views.map((view) => (
+        <Menu.Item
+          key={view}
+          name={view}
+          active={activeView === view}
+          onClick={() => setActiveView(view)}
         />
-      </Form.Field>
-      <Form.Field>
-        <Checkbox
-          radio
-          label="Table View"
-          name="viewGroup"
-          value="table"
-          checked={activeView === 'table'}
-          onChange={(_, data) => setActiveView(data.value)}
-        />
-      </Form.Field>
-    </Form>
+      ))}
+    </Menu>
   )
 }
