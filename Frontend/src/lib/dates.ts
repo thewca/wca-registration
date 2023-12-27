a992ce47951081ce17fe9161ca933d5798c7a496
Finn Ickler <finnickler@gmail.com>
2023-12-27 17:41:36 +0100

    switch dates to typescript

--- Frontend/src/lib/dates.ts
+++ Frontend/src/lib/dates.ts
@@ -1,15 +1,24 @@
-import moment from 'moment'
+import { Activity } from '@wca/helpers'
+import { DateTime } from 'luxon'
 
-export const getDatesStartingOn = (startDate, numberOfDays, options) => {
+export const getDatesStartingOn = (
+  startDate: string,
+  numberOfDays: number,
+  options?: { offset: number }
+): Date[] => {
   const { offset } = options || { offset: 0 }
   const range = []
   for (let i = offset; i < numberOfDays + offset; i++) {
-    range.push(moment(startDate).add(i, 'days').toDate())
+    range.push(DateTime.fromISO(startDate).plus({ days: i }).toJSDate())
   }
   return range
 }
 
-export const activitiesByDate = (activities, date, timeZone) => {
+export const activitiesByDate = (
+  activities: Activity[],
+  date: Date,
+  timeZone: string
+) => {
   return activities.filter(
     // not sure how to check startTime *in timeZone* is on date, besides
     // comparing locale strings, which seems bad
@@ -21,14 +30,14 @@ export const activitiesByDate = (activities, date, timeZone) => {
   )
 }
 
-export const getShortTime = (date, timeZone) => {
+export const getShortTime = (date: string, timeZone: string) => {
   return new Date(date).toLocaleTimeString([], {
     timeStyle: 'short',
     timeZone,
   })
 }
 
-export const getLongDate = (date) => {
+export const getLongDate = (date: string) => {
   return new Date(date).toLocaleDateString([], {
     weekday: 'long',
     year: 'numeric',
