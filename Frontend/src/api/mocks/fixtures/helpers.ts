de97bbf58c5de9e3f7836fe9f130d8948c776c62
Finn Ickler <finnickler@gmail.com>
2023-12-27 19:07:41 +0100

    migrate factory helper

--- Frontend/src/api/mocks/fixtures/helpers.ts
+++ Frontend/src/api/mocks/fixtures/helpers.ts
@@ -1,9 +1,8 @@
-import moment from 'moment/moment'
+import { DateTime } from 'luxon'
 
 export function dateFromNow(months: number, days = 0) {
-  return moment()
-    .add(months, 'months')
-    .add(days, 'days')
+  return DateTime.now()
+    .plus({ months, days })
     .set({ hour: 19, minute: 0, second: 0, millisecond: 0 })
-    .toISOString()
+    .toISO()
 }
