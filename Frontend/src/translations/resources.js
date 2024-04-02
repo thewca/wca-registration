64a0430c39b6be446196d4f89c75e7870024ce8a
FinnIckler <finnickler@gmail.com>
2024-04-02 14:22:04 +0200

    update with latest en.json

--- Frontend/src/translations/resources.js
+++ Frontend/src/translations/resources.js
@@ -1,6 +1,7 @@
 import en from './en.json'
 
-// For development we only support english
+// For development we only support english. If you add a key
+// you need to add it locally and in the monolith repo
 const resources = { en: { translations: en.en } }
 
 export default resources
