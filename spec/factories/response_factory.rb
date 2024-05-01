6e02fd69747c5981f0ca1b1856c54eecfbfef02d
FinnIckler <finnickler@gmail.com>
2024-05-01 23:02:50 +0200

    add banned trait

--- spec/factories/response_factory.rb
+++ spec/factories/response_factory.rb
@@ -4,17 +4,19 @@ require 'factory_bot_rails'
 
 FactoryBot.define do
   factory :permissions_response, class: Hash do
-    transient do
-    end
 
     organized_competitions { [] }
 
-    can_attend_competitions { { 'scope' => "*" } }
-    can_organize_competitions { { 'scope' => organized_competitions} }
+    can_attend_competitions { { 'scope' => '*' } }
+    can_organize_competitions { { 'scope' => organized_competitions } }
     can_administer_competitions { { 'scope' => organized_competitions } }
 
     trait :admin do
       organized_competitions { "*" }
+    end
+
+    trait :banned do
+      can_attend_competitions { { 'scope' => [] } }
     end
 
     initialize_with { attributes.stringify_keys }
