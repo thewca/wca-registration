6d6ab81534b7a6789abb0dccd1936e4db9ffaadc
FinnIckler <finnickler@gmail.com>
2024-05-01 21:32:25 +0200

    remove Frontend

--- Frontend/src/api/helper/get_csrf_token.ts
+++
@@ -1,7 +0,0 @@
-export function getCSRFToken() {
-  const metaTag = document.querySelector('meta[name="csrf-token"]')
-  if (metaTag) {
-    return metaTag.getAttribute('content')
-  }
-  return null
-}
--- Frontend/src/api/mocks/get_jwt.ts
+++
@@ -1,8 +0,0 @@
-export const USER_KEY = 'user'
-
-export default async function getJWTMock(): Promise<string> {
-  const user = localStorage.getItem(USER_KEY)
-  const response = await fetch(`${process.env.AUTH_URL}/${user}`)
-  const data = await response.json()
-  return `Bearer: ${data.token}`
-}
