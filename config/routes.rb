2b7922488979af245f675d29c3f6cfaee83af5cf
FinnIckler <finnickler@gmail.com>
2024-05-01 21:39:03 +0200

    remove frontend test route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
     mount Rswag::Ui::Engine => '/api-docs'
     mount Rswag::Api::Engine => '/api-docs'
 
-    get '/test/jwt/:user_id', to: 'test#token'
     get '/test/reset', to: 'test#reset'
   end
 
