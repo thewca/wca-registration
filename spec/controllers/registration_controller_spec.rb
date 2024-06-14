c7c0f50cd3a639f821a631c2e83ac3f28e2aa93e
FinnIckler <finnickler@gmail.com>
2024-06-14 17:14:29 +0200

    stub mailer in bulk update

--- spec/controllers/registration_controller_spec.rb
+++ spec/controllers/registration_controller_spec.rb
@@ -55,6 +55,9 @@ describe RegistrationController do
   end
 
   describe '#bulk_update' do
+    before do
+      stub_request(:post, EmailApi.registration_email_path).to_return(status: 200, body: { emails_sent: 1 }.to_json)
+    end
     # TODO: Consider refactor into separate contexts with one expect() per it-block
     it 'returns a 422 if there are validation errors' do
       registration = FactoryBot.create(:registration)
