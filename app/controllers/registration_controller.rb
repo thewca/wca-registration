0619a118bf052cf4c1bc67a8f08c3d9a3f7a8138
FinnIckler <finnickler@gmail.com>
2024-06-14 16:50:10 +0200

    disable emails until other PR is merged

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -134,7 +134,7 @@ class RegistrationController < ApplicationController
     end
 
     if Rails.env.production?
-      EmailApi.send_update_email(@competition_id, user_id, status, @current_user)
+      # EmailApi.send_update_email(@competition_id, user_id, status, @current_user)
     end
 
     {
