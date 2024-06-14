3fd95d5aab1da01995913189dbb41abce325baa6
FinnIckler <finnickler@gmail.com>
2024-06-14 11:57:38 +0200

    change request factory

--- spec/factories/request_factory.rb
+++ spec/factories/request_factory.rb
@@ -90,8 +90,7 @@ FactoryBot.define do
     transient do
       user_ids { [] }
     end
-
-    submitted_by { 1306 }
+    competition_id { 'CubingZANationalChampionship2023' }
     jwt_token { fetch_jwt_token(submitted_by) }
     requests do
       user_ids.map do |user_id|
