1bd954d20e72195a13e25bd04ab1a672eee6081c
FinnIckler <finnickler@gmail.com>
2024-07-24 23:28:17 +0200

    add import route

--- app/controllers/internal_controller.rb
+++ app/controllers/internal_controller.rb
@@ -82,4 +82,60 @@ class InternalController < ApplicationController
     registration = Registration.find(attendee_id)
     render json: registration
   end
+
+  def import_registrations
+    competition_id = params.require(:competition_id)
+    to_create = params[:create] || []
+    to_update = params[:update] || []
+    to_delete = params[:delete] || []
+    if to_create.any?
+      puts(to_create.map { |r| { attendee_id: r["attendee_id"], entries: [{ 'changed_attributes' =>
+                                                                { event_ids: r["event_ids"], status: r["competing_status"] },
+                                                              'actor_type' => r["actor_type"],
+                                                              'actor_id' => r["actor_id"],
+                                                              'action' => 'Import',
+                                                              'timestamp' => Time.now }] } }.inspect)
+      RegistrationHistory.import(to_create.map do |r|
+        {
+          attendee_id: r["attendee_id"],
+          entries: [History.new({
+                      'changed_attributes' => {
+                        event_ids: r["event_ids"],
+                        status: r["competing_status"]
+                      },
+                      'actor_type' => r["actor_type"],
+                      'actor_id' => r["actor_id"],
+                      'action' => 'Import',
+                      'timestamp' => Time.now
+                    })]
+        }
+      end)
+      Registration.import(to_create.map do |r|
+        {
+          attendee_id: r["attendee_id"],
+          competition_id: competition_id,
+          user_id: r["user_id"],
+          competing_status: r["competing_status"],
+          lanes: [
+            LaneFactory.competing_lane(
+              event_ids: r["event_ids"],
+              registration_status: r["competing_status"]
+            )
+          ]
+        }
+      end)
+    end
+    to_update.each do |r|
+      registration = Registration.find(r["attendee_id"])
+      registration.update(r["attendee_id"], competing_status: r["competing_status"], lanes: [LaneFactory.competing_lane(event_ids: r["event_ids"], registration_status: r["competing_status"])])
+      registration.history.add_entry({ event_ids: r["event_ids"], status: r["competing_status"] }, r["actor_type"], r["actor_id"], "import")
+    end
+    to_delete.each do |r|
+      Registration.update(r["attendee_id"], competing_status: "cancelled", lanes: [LaneFactory.competing_lane(event_ids: r["event_ids"], registration_status: "cancelled" )])
+      registration.history.add_entry({ event_ids: r["event_ids"], status: "cancelled" }, r["actor_type"], r["actor_id"], "import")
+    end
+  render json: { errors: [] }
+  rescue StandardError => e
+    return render json: { errors: [e.to_s] }, status: :unprocessable_entity
+  end
 end
