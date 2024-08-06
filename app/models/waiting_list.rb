da4002388505efd512001907787aff37fcc01a48
FinnIckler <finnickler@gmail.com>
2024-08-06 16:52:57 -0400

    add waiting list model

---
+++ app/models/waiting_list.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+class WaitingList
+  include Dynamoid::Document
+
+  # We autoscale dynamodb
+  table name: EnvConfig.WAITING_LIST_DYNAMO_TABLE, capacity_mode: nil, key: :id
+
+  field :entries, :array, of: :integer
+
+  def remove_competitor(competitor_id)
+    update_attributes!(entries: entries - [competitor_id])
+  end
+
+  def add_competitor(competitor_id)
+    if entries.nil?
+      update_attributes!(entries: [competitor_id])
+    else
+      update_attributes!(entries: entries + [competitor_id])
+    end
+  end
+
+  def move_competitor(old_index, new_index)
+    update_attributes!(entries: entries.insert(old_index, entries.delete_at(new_index)))
+  end
+end
