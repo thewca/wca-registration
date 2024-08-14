0b18db15768b9ab4c9b70a97ebdb6031a119027b
Duncan <duncanonthejob@gmail.com>
2024-08-14 09:26:26 +0200

    rubocop

--- spec/support/stub_helper.rb
+++ spec/support/stub_helper.rb
@@ -8,7 +8,7 @@ def stub_json(url, response_code, payload, type = :get)
   )
 end
 
-def stub_qualifications(payload=nil, qualification_data_date=nil)
+def stub_qualifications(payload = nil, qualification_data_date = nil)
   url_regex = %r{#{Regexp.escape(EnvConfig.WCA_HOST)}/api/v0/results/\d+/qualification_data(\?date=\d{4}-\d{2}-\d{2})?}
   stub_request(:get, url_regex).to_return do |request|
     uri = URI(request.uri)
