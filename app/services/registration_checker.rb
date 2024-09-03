9d7c6d567eb40c60c8dc19421dfb5d0fa0d91e69
FinnIckler <finnickler@gmail.com>
2024-09-03 12:10:44 +0200

    allow admins to move people to waiting list if User edits are not allowed

--- app/services/registration_checker.rb
+++ app/services/registration_checker.rb
@@ -71,7 +71,7 @@ class RegistrationChecker
 
     def user_can_modify_registration!
       raise RegistrationError.new(:unauthorized, ErrorCodes::USER_INSUFFICIENT_PERMISSIONS) unless can_administer_or_current_user?
-      raise RegistrationError.new(:forbidden, ErrorCodes::USER_EDITS_NOT_ALLOWED) unless @competition_info.registration_edits_allowed? || @competition_info.is_organizer_or_delegate?(@requester_user_id)
+      raise RegistrationError.new(:forbidden, ErrorCodes::USER_EDITS_NOT_ALLOWED) unless @competition_info.registration_edits_allowed? || UserApi.can_administer?(@requester_user_id, @competition_info.id)
       raise RegistrationError.new(:forbidden, ErrorCodes::ALREADY_REGISTERED_IN_SERIES) if existing_registration_in_series?
     end
 
