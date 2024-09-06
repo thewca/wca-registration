dd74da4c5574b4681ce3ca4a218dcdcc58add596
FinnIckler <finnickler@gmail.com>
2024-09-06 15:52:53 +0200

    run rubocop

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -2,9 +2,9 @@
 
 class ErrorsController < ApplicationController
   def show
-    exception = request.env["action_dispatch.exception"]
+    exception = request.env['action_dispatch.exception']
     status_code = ActionDispatch::ExceptionWrapper.new(request.env, exception).status_code
-    request_id = request.env["action_dispatch.request_id"]
+    request_id = request.env['action_dispatch.request_id']
     render json: { error: ErrorCodes::INTERNAL_SERVER_ERROR, request_id: request_id }, status: status_code
   end
 end
