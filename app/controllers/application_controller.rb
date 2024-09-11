a290a81941d779852fb50059ebd32d0c7bbe7438
FinnIckler <finnickler@gmail.com>
2024-09-11 13:07:02 +0200

    move all metrics to new relic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < ActionController::API
       decoded_token = (JWT.decode token, JwtOptions.secret, true, { algorithm: JwtOptions.algorithm })[0]
       @current_user = decoded_token['user_id'].to_i
     rescue JWT::VerificationError, JWT::InvalidJtiError
-      Metrics.jwt_verification_error_counter.increment
+      Metrics.increment("jwt_verification_error_counter")
       render json: { error: ErrorCodes::INVALID_TOKEN }, status: :unauthorized
     rescue JWT::ExpiredSignature
       render json: { error: ErrorCodes::EXPIRED_TOKEN }, status: :unauthorized
@@ -37,7 +37,7 @@ class ApplicationController < ActionController::API
   end
 
   def render_error(http_status, error, data = nil)
-    Metrics.registration_validation_errors_counter.increment
+    Metrics.increment("registration_validation_errors_counter")
     if data.present?
       render json: { error: error, data: data }, status: http_status
     else
--- lib/metrics.rb
+++ lib/metrics.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 module Metrics
-  class << self
-    attr_accessor :registration_validation_errors_counter, :registration_dynamodb_errors_counter, :registrations_counter, :registration_competition_api_error_counter, :registration_competitor_api_error_counter,
-                  :registration_impersonation_attempt_counter, :jwt_verification_error_counter
+  def self.increment(metric)
+    return if Rails.env.local?
+    ::NewRelic::Agent.increment_metric(metric)
   end
 end
