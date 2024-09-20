de2d5d322b909c3dd53059363097edbc6299f604
Duncan <duncanonthejob@gmail.com>
2024-09-20 17:16:30 +0200

    rubocop

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -10,7 +10,7 @@ class Registration
 
   REGISTRATION_STATES = %w[pending waiting_list accepted cancelled rejected].freeze
   ADMIN_ONLY_STATES = %w[pending waiting_list accepted rejected].freeze # Only admins are allowed to change registration state to one of these states
-  MIGHT_ATTEND_STATES = %w[pending waiting_list accepted]
+  MIGHT_ATTEND_STATES = %w[pending waiting_list accepted].freeze
 
   # Pre-validations
   before_validation :set_competing_status
