7344c8b5e2d333e26c8b1015c2518858ae1179d2
Gregor Billing <gregor.billing.dev@gmail.com>
2024-09-21 22:20:42 +0900

    Add hook to delay email sending in registration queue

--- app/jobs/registration_processor.rb
+++ app/jobs/registration_processor.rb
@@ -7,22 +7,25 @@ class RegistrationProcessor < ApplicationJob
 
   def perform(message)
     Rails.logger.debug { "Working on Message: #{message}" }
+    hooks = JobHooks.new
     if message[:step] == 'EventRegistration'
       event_registration(message[:competition_id],
                          message[:user_id],
                          message[:step_details][:event_ids],
                          message[:step_details][:comment],
                          message[:step_details][:guests],
-                         message[:created_at])
+                         message[:created_at],
+                         hooks)
     end
     Rails.cache.delete("#{message[:user_id]}-registrations-by-user")
+    hooks.run(:after_processing)
     Metrics.increment('registrations_processed')
   end
 
   private
 
     # rubocop:disable Metrics/ParameterLists
-    def event_registration(competition_id, user_id, event_ids, comment, guests, created_at)
+    def event_registration(competition_id, user_id, event_ids, comment, guests, created_at, hooks)
       # Event Registration might not be the first lane that is completed
       # TODO: When we add another lane, we need to update the registration history instead of creating it
       registration = begin
@@ -46,7 +49,9 @@ class RegistrationProcessor < ApplicationJob
       else
         registration.update_attributes(lanes: registration.lanes.append(competing_lane), guests: guests)
       end
-      EmailApi.send_creation_email(competition_id, user_id)
+      hooks.after_processing do
+        EmailApi.send_creation_email(competition_id, user_id)
+      end
     end
   # rubocop:enable Metrics/ParameterLists
 end
